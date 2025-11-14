0b75c852b20653b0e93c5c99830650ad5438faef
nicolas carrara <nicolas.carrara1u@gmail.com>
2025-11-14 11:32:40 +0100

    ok

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+source .env
 
 # Start Flask in background
 echo "Starting Flask..."
@@ -7,7 +8,7 @@ poetry run python upload_server.py &
 
 # Start Streamlit in background
 echo "Starting Streamlit..."
-poetry run streamlit run app.py --server.port=8501 --server.address=0.0.0.0 &
+poetry run streamlit run app.py --server.port=3000 --server.address=0.0.0.0 &
 
 # Wait for all background processes to finish
 wait
